83743ba0b31406f86ad4d0d6dc1267d5d04f09cd
KoganezawaRyouta <ryota.koganezawa@gmail.com>
2019-08-29 11:12:36 +0900

    first commit

--- ios-sample-x/ViewController.swift
+++ ios-sample-x/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import Geth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // ファイルを保存するディレクトリのパスを取得
+        let dataDir = NSSearchPathForDirectoriesInDomains(.documentDirectory,
+                                                          .userDomainMask,
+                                                          true)[0]
+        let keyStorePath = dataDir + "/keystore"
+        print("keyStorePath: \(keyStorePath)")
+
+        // keystore を管理してくれるやつのインスタンスを取得
+        let keyStoreManager = GethNewKeyStore(keyStorePath, GethLightScryptN, GethLightScryptP)
+
+        // アカウント作成
+        let account = try! keyStoreManager?.newAccount("password")
+
+        // アドレスを取得
+        let address = account?.getAddress().getHex()
+        print("address: \(address!)")
+
+        // keystore ファイル URL
+        let url = account?.getURL()
+        print("url: \(url!)")
     }
-
-
 }
 
